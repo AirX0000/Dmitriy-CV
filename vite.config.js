00a1ee2d190f97f8c2fd96830e820c8e12facd8d
ㅤAir ㅤ <air@MacBook-Air-Air.local>
2026-02-18 11:06:44 +0500

    ci: add GitHub Actions workflow for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/Dmitriy-CV/',
     server: { port: 5173, host: true }
 })
